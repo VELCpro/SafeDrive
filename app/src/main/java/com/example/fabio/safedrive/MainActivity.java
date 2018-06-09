57c086351367664f049235060ce1eee99acd96ef
VELCpro <fabiobellotti92@gmail.com>
2018-06-09 23:41:36 +0200

    Aggiornamento dopo il quarto video. salvato nella galleria le foto, aggiunto MediaScanner per aggiungere le foto alla galleria

--- app/src/main/java/com/example/fabio/safedrive/MainActivity.java
+++ app/src/main/java/com/example/fabio/safedrive/MainActivity.java
@@ -1,9 +1,19 @@
 package com.example.fabio.safedrive;
 
 import android.hardware.Camera;
+import android.media.MediaScannerConnection;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.FrameLayout;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,5 +38,58 @@ public class MainActivity extends AppCompatActivity {
         frameLayout.addView(showCamera);
 
 
+    }
+
+    Camera.PictureCallback mPictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File picture_file = getOutputMediaFile();
+
+            if(picture_file == null){
+                return;
+            }else{
+                try {
+                    FileOutputStream fos = new FileOutputStream(picture_file);
+                    fos.write(data);
+                    fos.close();
+                    MediaScannerConnection.scanFile(getApplicationContext(), new String[] { picture_file.getPath() }, new String[] { "image/jpeg" }, null);
+
+                    camera.startPreview();
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    };
+
+    private File getOutputMediaFile() {
+     // Creo un nome unico per ogni foto associandogli un TimeStamp
+        String formato = "jpg";
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + ".";
+        String state = Environment.getExternalStorageState();
+        if(!state.equals(Environment.MEDIA_MOUNTED)){
+            return null;
+        }
+        else{
+            File folder_gui = new File(Environment.getExternalStorageDirectory() + File.separator + "GUI");
+
+            if(!folder_gui.exists()){
+                folder_gui.mkdirs();
+            }
+
+            File outputFile = new File(folder_gui,imageFileName + formato);
+            return outputFile;
+        }
+    }
+
+    public void captureImage(View v){
+        if(camera != null){
+            camera.takePicture(null,null,mPictureCallback);
+        }
     }
 }
